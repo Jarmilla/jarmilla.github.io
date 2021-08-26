8eade3d0b13380c0b65ce2c7b92303a40dfc0f8f
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-08-26 15:29:16 +0200

    ProjectDisplay.js showing data

--- jarmilla.github.io/src/components/ProjectDisplay.js
+++ jarmilla.github.io/src/components/ProjectDisplay.js
@@ -1,9 +1,14 @@
-import react from "react";
-
 function ProjectDisplay({ project }) {
   return (
     <div>
       <h1>{project.name}</h1>
+      <p>{project.description}</p>
+      <ul>
+        {project.features.map((feature) => (
+          <li>{feature}</li>
+        ))}
+      </ul>
+      <img src={project.image} alt={project.name} />
     </div>
   );
 }
