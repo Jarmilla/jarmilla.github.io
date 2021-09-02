cca7b50b5f031bf25f8808dad7fd0102e190e075
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-09-02 17:30:30 +0200

    Project.js data expanded, typo fixed

--- jarmilla.github.io/src/data/Projects.js
+++ jarmilla.github.io/src/data/Projects.js
@@ -9,40 +9,41 @@ import startrekBG from "../img/dom-talbot-ZuFxrKGzAj4-unsplash.jpg";
 export const projects = [
   {
     name: "Astronomy Pictures Of the Day",
-    description: "Each day a different image or photograph of our universe is featured, along with a brief explanation written by a professional astronomer.",
-    features: ["Fetched from an API (Nasa APOD)", "Preview of the previous 6 day pictures", "Search options for previous dates."],
+    description: "Each day a different image or video of our universe is featured, along with a brief explanation written by a professional astronomer.",
+    features: ["Fetched from an API (Nasa APOD)", "Preview pictures of the previous 6 day ", "Search options for previous dates."],
     ghPages: "https://jarmilla.github.io/nasa-open-API/",
     github: "https://github.com/Jarmilla/nasa-open-API",
     image: nasaPic,
     background: {
       img: nasaBG,
-      alt: "Photo by NASA on Unsplash",
+      createdBy: "Photo by NASA on Unsplash",
     },
   },
   {
     name: "Ryuutama RPG Character Sheet",
     description:
-      "At one point, in everyone’s lives, people get this intense feeling of wanderlust. They put their daily lives on hold and travel the world with new-found companions. They find out more about the world, and at the same time learn about themselves.",
+      "Online editable character sheet for Ryuutama. Ryuutama is a light hearted tabletop roleplaying game, where you can play with regular people whose are going on an adventure.",
     features: ["Create and edit your Ryuutama character", "Saving to localStorage", "Roll your checks for the game"],
     ghPages: "https://jarmilla.github.io/ryuutama-rpg/",
     github: "https://github.com/Jarmilla/ryuutama-rpg",
     image: ryuutama,
     background: {
       img: forestBG,
-      alt: "Photo by J-Photos on Unsplash",
+      createdBy: "Photo by J-Photos on Unsplash",
     },
   },
   {
     name: "Sci-Fi Scanner",
-    description: "Search for QR codes, find your way to the next location, complite the given tasks, just like in Star Trek.",
-    used: "Built for a sci-fi themed searching game. Got positive feedbacks from participants.",
-    features: ["QR code scanner", "Weather forecast", "Optimized for mobile devices"],
+    description:
+      "Your group is tasked to land and investigate on the nearby planet. Search for QR codes, find your way to the next location, complete the given tasks, just like in Star Trek.",
+    used: "Built for a sci-fi themed searching game for groups. Got positive feedbacks from participants.",
+    features: ["QR code scanner", "Weather forecast", "Optimized for mobile devices", "Data stored in easy to change JSON file"],
     ghPages: "https://jarmilla.github.io/adamas-app/",
     github: "https://github.com/Jarmilla/adamas-app",
     image: adamis,
     background: {
       img: startrekBG,
-      alt: "Photo by Dom Talbot on Unsplash",
+      createdBy: "Photo by Dom Talbot on Unsplash",
     },
   },
 ];
