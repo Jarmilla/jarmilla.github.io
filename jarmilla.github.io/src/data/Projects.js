768dd0a8026e4a825743a4ba1fb382fe2edc141e
Jarmilla <bodorne.deme.adrienn@gmail.com>
2021-09-28 14:31:39 +0200

    changing project sequence

--- jarmilla.github.io/src/data/Projects.js
+++ jarmilla.github.io/src/data/Projects.js
@@ -8,15 +8,17 @@ import startrekBG from "../img/dom-talbot-ZuFxrKGzAj4-unsplash.jpg";
 
 export const projects = [
   {
-    name: "Astronomy Pictures Of the Day",
-    description: "Each day a different image or video of our universe is featured, along with a brief explanation written by a professional astronomer.",
-    features: ["Fetched from an API (Nasa APOD)", "Preview pictures of the previous 6 day ", "Search options for previous dates."],
-    ghPages: "https://jarmilla.github.io/nasa-open-API/",
-    github: "https://github.com/Jarmilla/nasa-open-API",
-    image: nasaPic,
+    name: "Sci-Fi Scanner",
+    description:
+      "Your group is tasked to land and investigate on the nearby planet. Search for QR codes, find your way to the next location, complete the given tasks, just like in Star Trek.",
+    used: "Built for a sci-fi themed searching game for groups. Got positive feedbacks from participants.",
+    features: ["QR code scanner", "Weather forecast", "Optimized for mobile devices", "Data stored in easy to change JSON file"],
+    ghPages: "https://jarmilla.github.io/adamas-app/",
+    github: "https://github.com/Jarmilla/adamas-app",
+    image: adamis,
     background: {
-      img: nasaBG,
-      createdBy: "Photo by NASA on Unsplash",
+      img: startrekBG,
+      createdBy: "Photo by Dom Talbot on Unsplash",
     },
   },
   {
@@ -33,17 +35,15 @@ export const projects = [
     },
   },
   {
-    name: "Sci-Fi Scanner",
-    description:
-      "Your group is tasked to land and investigate on the nearby planet. Search for QR codes, find your way to the next location, complete the given tasks, just like in Star Trek.",
-    used: "Built for a sci-fi themed searching game for groups. Got positive feedbacks from participants.",
-    features: ["QR code scanner", "Weather forecast", "Optimized for mobile devices", "Data stored in easy to change JSON file"],
-    ghPages: "https://jarmilla.github.io/adamas-app/",
-    github: "https://github.com/Jarmilla/adamas-app",
-    image: adamis,
+    name: "Astronomy Pictures Of the Day",
+    description: "Each day a different image or video of our universe is featured, along with a brief explanation written by a professional astronomer.",
+    features: ["Fetched from an API (Nasa APOD)", "Preview pictures of the previous 6 day ", "Search options for previous dates."],
+    ghPages: "https://jarmilla.github.io/nasa-open-API/",
+    github: "https://github.com/Jarmilla/nasa-open-API",
+    image: nasaPic,
     background: {
-      img: startrekBG,
-      createdBy: "Photo by Dom Talbot on Unsplash",
+      img: nasaBG,
+      createdBy: "Photo by NASA on Unsplash",
     },
   },
 ];
